785761967b222af140c3ed6f9e63ec0cd9d0bcbc
Hugo Rodrigues Pereira <hugo999junha@gmail.com>
2022-02-05 19:53:57 -0300

    feat: Add manipulation of database config window
    Add the control of database config window of genereted file by uic6

---
+++ windows/database_config_window.py
@@ -0,0 +1,63 @@
+from socketserver import ThreadingUDPServer
+from PyQt6 import QtWidgets, QtCore, QtGui, uic
+from windows_ui.database_config_window import Ui_DataBaseConfigWindow
+
+
+class DataBaseConfigWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super(DataBaseConfigWindow, self).__init__(*args, **kwargs)
+
+        # Loading the database config window
+        self.window_ui: Ui_DataBaseConfigWindow = Ui_DataBaseConfigWindow()
+        # insert the QMainWindow into the database config window
+        self.window_ui.setupUi(self)
+
+        # Loading the dependences  of window
+        #---------------------------------------------------
+        self.loading_depedences()
+        #---------------------------------------------------
+
+    def create_window(self) -> None:
+        
+        self.window_ui.frame_show_configs.hide()
+        self.window_ui.frame_progress_bar.hide()
+        self.setWindowTitle("Config DataBase")
+        self.show()
+    
+    def change_echo_mode(self) -> None:
+
+        if self.window_ui.line_edit_password.echoMode() == QtWidgets.QLineEdit.EchoMode.Password:
+            self.window_ui.line_edit_password.setEchoMode(QtWidgets.QLineEdit.EchoMode.Normal)
+        
+        else:
+            self.window_ui.line_edit_password.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+
+    def loading_depedences(self) -> None:
+
+        # Change the max-size of frame_show_configs
+        self.window_ui.frame_show_configs.setMaximumSize(QtCore.QSize(16777215, 16777215))
+
+        # Change echo mode pedendence
+        #---------------------------------------------------
+        self.window_ui.push_button_show_hide_password.pressed.connect(self.change_echo_mode)
+        #---------------------------------------------------
+
+        # Open frames dependences
+        #---------------------------------------------------
+        self.window_ui.push_button_show_configs.pressed.connect(self.open_show_configs)
+
+        self.window_ui.push_button_setting_database.pressed.connect(self.open_setting_database)
+        #---------------------------------------------------
+    
+
+    def open_show_configs(self) -> None:
+
+        self.window_ui.frame_setting_database.hide()
+        self.window_ui.frame_show_configs.show()
+    
+    def open_setting_database(self) -> None:
+
+        self.window_ui.frame_show_configs.hide()
+        self.window_ui.frame_setting_database.show()
+
